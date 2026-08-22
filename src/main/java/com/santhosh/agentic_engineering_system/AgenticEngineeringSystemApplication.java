77335f0dec6b1c62d7f5d2bb0897364fe3a450a0
Santosh ch <chsantoshk88@gmail.com>
2026-08-22 13:26:22 -0500

    chore: bootstrap agentic engineering platform

---
+++ src/main/java/com/santhosh/agentic_engineering_system/AgenticEngineeringSystemApplication.java
@@ -0,0 +1,13 @@
+package com.santhosh.agentic_engineering_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenticEngineeringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenticEngineeringSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santhosh/agentic_engineering_system/AgenticEngineeringSystemApplicationTests.java
@@ -0,0 +1,14 @@
+package com.santhosh.agentic_engineering_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AgenticEngineeringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
